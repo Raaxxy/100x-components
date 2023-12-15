e48630fcafb76875b62bf6cf28367850ac4ad42b
Raaxxy <rahularyan0197@gmail.com>
2023-12-15 05:44:35 +0530

    Build button Componets

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,12 +18,12 @@ module.exports = {
         "Neutral-100":"#F4F4F4",
         "Neutral-50": "#F9F9F9",
         "gray":"rgba(255,255,255,0.12)",
-        "twitter blue-default":"#1D9BF0",
-        "twitter blue-hover":"#1871CA",
-        "twitter blue-disabled":"#1E5D87",
+        "twitter-blue-default":"#1D9BF0",
+        "twitter-blue-hover":"#1871CA",
+        "twitter-blue-disabled":"#1E5D87",
         "twitter-white-100": "#FFF",
         "blue-wash":"rgba(117,190,239,0.2)",
-        "button stroke": "#546A7A",
+        "button-stroke": "#546A7A",
         "searchbar-fill":"#212327",
         "card-fill":"#16181C",
         "green-success":"#00BE74",
@@ -34,38 +34,37 @@ module.exports = {
         "inter": ['Inter', 'sans-serif']
       },
       borderRadius: {
-        'button-l': "30px",
-        'button-xl': "65px",
+        "border-radius-65": "65px",
+        "border-radius-30": "30px",
+        "border-radius-9999": "9999px",
+        "border-radius-4": "4px"
       },
       boxShadow: {
         'button': '0px 8px 16px 0px rgba(0, 0, 0, 0.25)',
       },
-      spacing:{
-        0.25:'1px',
-        1.25:'5px',
-        4.5:'18px',
-        16.5:'66px',
-        23.5:'93px',
-        35.75:'143px',
-        39:'156px',
-        43:'172px',
-        50:'200px',
-        58.25:'233px',
-        87:'348px',
-        88.75:'355px',
-        98.5:'394px',
-        100.5:'402px',
-        106.5:'426px',
-        108.5:'434px',
-        120.5:'482px',
-        128:'512px',
-        148.25:'593px',
-        150:'600px',
+      backdropBlur: {
+        "23": "blur(23.668209075927734px)"
+      },
+      width: {
+        "334": "334px",
+        "348": "348px",
+        "442": "442px",
+        "426": "426px",
+        "482": "482px",
+        "434": "434px",
+      },
+      height: {
+        "39": "39px"
+      },
+      lineHeight: {
+        "normal": "normal"
+      },
+      borderColor: {
+        'border-stroke': 'var(--stroke, rgba(29, 155, 240, 0.24))',
       }
+
     },
     
   },
-  corePlugins:{
-    preflight: false,
-  },
+  plugins: [],
 };
